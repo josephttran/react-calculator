44e2246d4c221cff92bcdd7915a6e9434852bb85
josephttran <42635012+josephttran@users.noreply.github.com>
2018-12-02 19:53:48 -0800

    handle number click

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,29 @@ import './App.css';
 import Button from './Button/Button';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 0,
+    };
+    this.handleNumberClick = this.handleNumberClick.bind(this);
+  }
+
+  handleNumberClick(i) {
+    if (this.state.value == 0) {
+      this.setState({value: i});
+    } else {
+      this.setState({value: this.state.value + i});
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Calculator
         </header>
-        <div className="display">0</div>
+        <div className="display">{this.state.value}</div>
         <div className="operator">
           <Button id="add" label='+' />
           <Button id="subtract" label='-' />
@@ -19,16 +35,16 @@ class App extends Component {
           <Button id="equals" label='=' />                
         </div>
         <div className="digit">
-          <Button id="zero" label='0' />
-          <Button id="one" label='1' />
-          <Button id="two" label='2' />
-          <Button id="three" label='3' />
-          <Button id="four" label='4' />
-          <Button id="five" label='5' />
-          <Button id="six" label='6' />
-          <Button id="seven" label='7' />
-          <Button id="eight" label='8' />
-          <Button id="nine" label='9' />
+          <Button id="zero" label='0' handleClick={this.handleNumberClick}/>
+          <Button id="one" label='1'  handleClick={this.handleNumberClick}/>
+          <Button id="two" label='2'  handleClick={this.handleNumberClick}/>
+          <Button id="three" label='3'  handleClick={this.handleNumberClick}/>
+          <Button id="four" label='4'  handleClick={this.handleNumberClick}/>
+          <Button id="five" label='5'  handleClick={this.handleNumberClick}/>
+          <Button id="six" label='6'  handleClick={this.handleNumberClick}/>
+          <Button id="seven" label='7'  handleClick={this.handleNumberClick}/>
+          <Button id="eight" label='8'  handleClick={this.handleNumberClick}/>
+          <Button id="nine" label='9'  handleClick={this.handleNumberClick}/>
         </div>
         <div className="decimal">
           <Button id="decimal" label="." />
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ import './button.css';
 class Button extends Component {
   render() {
     return (
-      <button>
+      <button onClick={() => this.props.handleClick(this.props.label)}>
         {this.props.label}
       </button>
     )
