3008236b9535f8bd5517ecef4e70fd5993c39302
josephttran <42635012+josephttran@users.noreply.github.com>
2018-12-02 20:46:05 -0800

    handle decimal, clear button

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,41 @@ class App extends Component {
       value: 0,
     };
     this.handleNumberClick = this.handleNumberClick.bind(this);
+    this.handleOpClick = this.handleOpClick.bind(this);
   }
 
   handleNumberClick(i) {
-    if (this.state.value == 0) {
+    if (this.state.value === 0 && i == 0) {
+
+    } else if (this.state.value === 0 && i != 0) {
       this.setState({value: i});
     } else {
       this.setState({value: this.state.value + i});
     }
+  }
+
+  handleOpClick(op) {
+    this.setState({operator: op});
+  }
+
+  handleDecimal = () => {
+    let num = this.state.value;
+
+    if (num === 0) {
+      num += '.';    
+      this.setState({value: num})
+    } else {
+      if (num.indexOf('.') === -1) {
+        num += '.';
+        this.setState({value: num})
+      }
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      value: 0
+    })
   }
 
   render() {
@@ -35,22 +62,22 @@ class App extends Component {
           <Button id="equals" label='=' />                
         </div>
         <div className="digit">
-          <Button id="zero" label='0' handleClick={this.handleNumberClick}/>
-          <Button id="one" label='1'  handleClick={this.handleNumberClick}/>
-          <Button id="two" label='2'  handleClick={this.handleNumberClick}/>
-          <Button id="three" label='3'  handleClick={this.handleNumberClick}/>
-          <Button id="four" label='4'  handleClick={this.handleNumberClick}/>
-          <Button id="five" label='5'  handleClick={this.handleNumberClick}/>
-          <Button id="six" label='6'  handleClick={this.handleNumberClick}/>
-          <Button id="seven" label='7'  handleClick={this.handleNumberClick}/>
-          <Button id="eight" label='8'  handleClick={this.handleNumberClick}/>
-          <Button id="nine" label='9'  handleClick={this.handleNumberClick}/>
+          <Button id="zero" label='0' handleClick={this.handleNumberClick} />
+          <Button id="one" label='1'  handleClick={this.handleNumberClick} />
+          <Button id="two" label='2'  handleClick={this.handleNumberClick} />
+          <Button id="three" label='3'  handleClick={this.handleNumberClick} />
+          <Button id="four" label='4'  handleClick={this.handleNumberClick} />
+          <Button id="five" label='5'  handleClick={this.handleNumberClick} />
+          <Button id="six" label='6'  handleClick={this.handleNumberClick} />
+          <Button id="seven" label='7'  handleClick={this.handleNumberClick} />
+          <Button id="eight" label='8'  handleClick={this.handleNumberClick} />
+          <Button id="nine" label='9'  handleClick={this.handleNumberClick} />
         </div>
         <div className="decimal">
-          <Button id="decimal" label="." />
+          <Button id="decimal" label="." handleClick={this.handleDecimal} />
         </div>
         <div className="clear">
-          <Button id="clear" label="clear" />
+          <Button id="clear" label="clear" handleClick={this.clear} />
         </div>
       </div>
     );
