8b9ad3ae3f4bce8ad5e1dd9701719134430f7bb4
josephttran <42635012+josephttran@users.noreply.github.com>
2018-12-03 22:47:31 -0800

    evaluate formula

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Button from './Button/Button';
+import evaluate from '../util/evaluate';
 
 class App extends Component {
   constructor(props){
@@ -15,6 +16,47 @@ class App extends Component {
     };
     this.handleNumberClick = this.handleNumberClick.bind(this);
     this.handleOpClick = this.handleOpClick.bind(this);
+  }
+
+  calculate = (equals) => {
+    if (this.state.formulaDisplay.indexOf(equals) === -1) {
+      let output = 0;
+      let expression = '';
+
+      if (!this.state.lastClickIsOp) {
+        expression = this.state.formulaDisplay + this.state.currValue;
+      } else {
+        expression = this.state.formulaDisplay;
+        expression = expression.slice(0, -1); 
+      }
+
+      output = evaluate(expression);
+
+      this.setState({
+        currValue: output,
+        operator: '=',
+        formulaDisplay: expression + equals + output,
+      });      
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      currValue: 0,
+      prevValue: 0,
+      operator: '',
+      formulaDisplay: '',
+      lastClickIsOp: false
+    })
+  }
+
+  handleDecimal = () => {
+    let num = this.state.currValue;
+
+    if (num.toString().indexOf('.') === -1) {
+      num += '.';
+      this.setState({currValue: num})
+    }
   }
 
   handleNumberClick(i) {
@@ -36,6 +78,10 @@ class App extends Component {
   }
 
   handleOpClick(op) {
+    if (this.state.formulaDisplay.indexOf('=') > -1) {
+
+    }
+
     let formula = '';  
    
     if (this.state.prevValue === 0) {
@@ -56,32 +102,13 @@ class App extends Component {
     })
   }
 
-  handleDecimal = () => {
-    let num = this.state.currValue;
-
-    if (num.toString().indexOf('.') === -1) {
-      num += '.';
-      this.setState({currValue: num})
-    }
-  }
-
-  clear = () => {
-    this.setState({
-      currValue: 0,
-      prevValue: 0,
-      operator: '',
-      formulaDisplay: '',
-      lastClickIsOp: false
-    })
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Calculator
         </header>
-        <div className="display">
+        <div id="display">
           <div className="formula">{this.state.formulaDisplay}</div>
           <div className="input">{this.state.currValue}</div>
         </div>
@@ -90,7 +117,7 @@ class App extends Component {
           <Button id="subtract" label='-' handleClick={this.handleOpClick} />
           <Button id="multiply" label='*' handleClick={this.handleOpClick} />
           <Button id="divide" label='/' handleClick={this.handleOpClick} />
-          <Button id="equals" label='=' />                
+          <Button id="equals" label='=' handleClick={this.calculate} />                
         </div>
         <div className="digit">
           <Button id="zero" label={0} handleClick={this.handleNumberClick} />
